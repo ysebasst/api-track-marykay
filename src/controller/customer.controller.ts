ee6a92dc7ffc0d2d8ff6a4f5ad20e81b8e5cf24f
Sebastian Sierra <ysebasst@gmail.com>
2022-03-24 01:12:21 -0500

    change try catch customer api

--- src/controller/customer.controller.ts
+++ src/controller/customer.controller.ts
@@ -1,32 +1,57 @@
 import Customer from "../models/customer.model";
 
 export const getCustomers = async (req, res) => {
-  const customers = await Customer.find();
-  res.json(customers);
+  try {
+    const customers = await Customer.find();
+    res.json(customers);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: error.message });
+  }
 };
 
 export const createCustomer = async (req, res) => {
-  const data = req.body;
-  const newCustomer = await Customer.create(data);
-  res.json(newCustomer);
+  try {
+    const data = req.body;
+    const newCustomer = await Customer.create(data);
+    res.json(newCustomer);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: error.message });
+  }
 };
 
 export const getCustomer = async (req, res) => {
-  const id = req.params.id;
-  const customer = await Customer.findById(id);
-  res.json(customer);
+  try {
+    const id = req.params.id;
+    const customer = await Customer.findById(id);
+    res.json(customer);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: error.message });
+  }
 };
 
 export const updateCustomer = async (req, res) => {
-  const id = req.params.id;
-  const updateCustomer = await Customer.findByIdAndUpdate(id, req.body, {
-    new: true,
-  });
-  res.json(updateCustomer);
+  try {
+    const id = req.params.id;
+    const updateCustomer = await Customer.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updateCustomer);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: error.message });
+  }
 };
 
 export const deleteCustomer = async (req, res) => {
-  const id = req.params.id;
-  const deleteCustomer = await Customer.findByIdAndDelete(id);
-  res.json(deleteCustomer);
+  try {
+    const id = req.params.id;
+    const deleteCustomer = await Customer.findByIdAndDelete(id);
+    res.json(deleteCustomer);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: error.message });
+  }
 };
